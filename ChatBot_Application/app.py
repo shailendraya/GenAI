c31c1aab6e3d93681fc3713790652c4dd1824030
Shailendra Nagina <125447802+shailendraya@users.noreply.github.com>
2024-10-07 14:01:08 +0530

    Demo Projects

---
+++ ChatBot_Application/app.py
@@ -0,0 +1,41 @@
+#Hello! It seems like you want to import the Streamlit library in Python. Streamlit is a powerful open-source framework used for building web applications with interactive data visualizations and machine learning models. To import Streamlit, you'll need to ensure that you have it installed in your Python environment.
+#Once you have Streamlit installed, you can import it into your Python script using the import statement,
+
+import streamlit as st
+
+from langchain_openai import OpenAI
+
+#When deployed on huggingface spaces, this values has to be passed using Variables & Secrets setting, as shown in the video :)
+#import os
+#os.environ["OPENAI_API_KEY"] = "sk-PLfFwPq6y24234234234FJ1Uc234234L8hVowXdt"
+
+#Function to return the response
+def load_answer(question):
+    llm = OpenAI(model_name="gpt-3.5-turbo-instruct",temperature=0)
+
+    answer=llm.invoke(question)
+    return answer
+
+
+#App UI starts here
+st.set_page_config(page_title="LangChain Demo", page_icon=":robot:")
+st.header("LangChain Demo")
+
+#Gets the user input
+def get_text():
+    input_text = st.text_input("You: ", key="input")
+    return input_text
+
+
+user_input=get_text()
+response = load_answer(user_input)
+
+submit = st.button('Generate')  
+
+#If generate button is clicked
+if submit:
+
+    st.subheader("Answer:")
+
+    st.write(response)
+
